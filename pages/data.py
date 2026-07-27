d68679989880f6f71d4962cb6952b5a36618b8b1
Jean-Charles Brunelle <nanasacplastiquehopoi@gmail.com>
2026-07-27 13:48:35 -1000

    Déplacement de la table questions dans Données brutes.

--- pages/data.py
+++ pages/data.py
@@ -13,11 +13,11 @@ st.dataframe(df_commune)
 st.subheader("Utilisateurs (profile)")
 st.dataframe(df_profile)
 
+st.subheader("Questions")
+st.dataframe(df_question)
+
 st.subheader("Sessions (score_session)")
 st.dataframe(df_score_session)
 
 st.subheader("Réponses (reponse_question)")
 st.dataframe(df_reponse_question)
-
-st.subheader("Questions")
-st.dataframe(df_question)
